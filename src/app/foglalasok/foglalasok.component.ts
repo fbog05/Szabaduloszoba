01d270acb40e2e9763839e8909aff6fd4af954a8
Boglarka <boglarkaferencsik05@gmail.com>
2024-01-06 16:26:31 +0100

    done

--- src/app/foglalasok/foglalasok.component.ts
+++ src/app/foglalasok/foglalasok.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FoglalasService } from '../foglalas.service';
 
 @Component({
-  selector: 'app-foglalasok',
+  selector: 'app-foglalas-lista',
   templateUrl: './foglalasok.component.html',
-  styleUrls: ['./foglalasok.component.css']
+  styleUrls: ['./foglalasok.component.css'],
 })
-export class FoglalasokComponent {
+export class FoglalasokComponent implements OnInit {
+  foglalasok: any[] = [];
+  displayedColumns: string[] = ['id', 'nev', 'iranyitoszam', 'cim', 'datum', 'fo'];
 
+  constructor(private foglalasService: FoglalasService) { }
+
+  ngOnInit(): void {
+    this.getFoglalasok();
+  }
+
+  getFoglalasok(): void {
+    this.foglalasService.getFoglalasok().subscribe({
+      next: (data) => {
+        console.log('Foglalások:', data);
+        this.foglalasok = data;
+      },
+      error: (error) => {
+        console.error('Hiba történt a foglalások lekérése során:', error);
+      }
+    });
+  }
 }
+
